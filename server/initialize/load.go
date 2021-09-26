15eb04ca32d5a34d34ebe7d7904554d9adeecb47
zchengo <1933757688@qq.com>
2021-09-26 13:23:09 +0800

    Initial commit

--- server/initialize/load.go
+++ server/initialize/load.go
@@ -8,11 +8,11 @@ import (
 
 // LoadConfig 加载配置文件
 func LoadConfig() {
-	viper.AddConfigPath("./")
+	viper.AddConfigPath("./server/")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	if err := viper.ReadInConfig(); err != nil {
-		panic(fmt.Errorf("Fatal error resources file: %w \n", err))
+		panic(fmt.Errorf("Fatal error resources file: %w \n", err))长度
 	}
 	if err := viper.Unmarshal(&global.Config); err != nil {
 		panic(fmt.Errorf("unable to decode into struct %w \n", err))
